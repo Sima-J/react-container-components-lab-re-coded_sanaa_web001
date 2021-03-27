1a5f41b9403715b3b6535c8dd832dec595d3bbf2
blessed-installation-48cc <sima.jazaa@gmail.com>
2021-03-27 10:34:12 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,9 @@
-// Code MovieReviews Here
+export default function MovieReviews(props) {
+    return (
+        <div className="review-list">
+            {props.reviews.map(review => { return <div className="review">
+        <h1>{review.display_title}</h1>
+        </div>})}
+        </div>
+    )
+}
